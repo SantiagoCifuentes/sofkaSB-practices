cc9b83efab793eafedfde4573573d73c62a271d3
Santiago C <santiago.cifuentes@sofka.com.co>
2023-10-20 14:57:18 -0500

    Initial commit

---
+++ src/test/java/com/sofkaSBpractices/SofkaSbPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaSBpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofkaSbPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
