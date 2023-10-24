1daf173fe3eb5d840b5c20e7f08a95090565554d
Santiago C <santiago.cifuentes@sofka.com.co>
2023-10-24 17:03:11 -0500

    Modelo de datos mapeado

--- src/main/java/com/sofkaSBpractices/persistence/entity/Producto.java
+++ src/main/java/com/sofkaSBpractices/persistence/entity/Producto.java
@@ -24,4 +24,70 @@ public class Producto {
 
     @Column(name = "cantidad_stock")
     private  Integer cantidadStock;
+
+    private Boolean estado;
+
+
+   //Un producto puede tener muchas categorías
+    @ManyToOne
+    @JoinColumn(name = "id_categoria" ,insertable = false , updatable = false) //
+    // el insertable y updatable significa que en esa relacion no se va a insertar o actualizar una nueva categoria
+    // si se le desea añadir , toca desde la clase categoría para el atributo
+    private Categoria categoria;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
