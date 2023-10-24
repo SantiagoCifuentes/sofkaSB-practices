e12e942be15d20c152b52f30ff6eb10077bde2bf
Santiago C <santiago.cifuentes@sofka.com.co>
2023-10-24 15:15:44 -0500

    Adición de conexión a bd

---
+++ src/main/java/com/sofkaSBpractices/persistence/entity/Producto.java
@@ -0,0 +1,27 @@
+package com.sofkaSBpractices.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //crea la clave primaria de manera automática
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private  Integer cantidadStock;
+}
