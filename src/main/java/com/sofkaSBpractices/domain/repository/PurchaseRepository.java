cc63c18ab713959b2553ed5971afb89913dc253c
Santiago C <santiago.cifuentes@sofka.com.co>
2023-10-30 11:16:34 -0500

    Implementación de los mapper de compras y comprasProducto

---
+++ src/main/java/com/sofkaSBpractices/domain/repository/PurchaseRepository.java
@@ -0,0 +1,14 @@
+package com.sofkaSBpractices.domain.repository;
+
+import com.sofkaSBpractices.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+
+}
---
+++ src/main/java/com/sofkaSBpractices/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,33 @@
+package com.sofkaSBpractices.persistence.mapper;
+
+import com.sofkaSBpractices.domain.PurchaseItem;
+import com.sofkaSBpractices.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = ProductMapper.class)
+public interface PurchaseItemMapper {
+
+    @Mappings({
+            @Mapping(source = "id.idProducto", target="productoId"),
+            @Mapping(source = "cantidad", target="quantity"),
+            //con total no se hace ya que se llama igual en la clase base y en la de dominio(maper)
+            @Mapping(source = "estado", target="active"),
+    })
+    PurchaseItem toPurchaseItem(ComprasProducto producto);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true),
+            @Mapping(target = "id.idCompra", ignore = true),
+    })
+    //esta conversión utiiliza de manera inversa los mappings definidos arriba
+    ComprasProducto toComprasProducto(PurchaseItem item);
+
+
+
+
+}
